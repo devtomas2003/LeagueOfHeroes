49fb6b19793c47927c902c3dbf99452704b3f561
Tomás Dinis <dev.tomas2003@gmail.com>
2023-12-27 15:13:39 +0000

    Key Correction

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-const env = dotenv.config().parsed;
 
 export default {
     api: axios.create({
         baseURL: 'https://psw-server.onrender.com',
         timeout: 5000
     }),
-    secret: env.API_KEY,
+    secret: 'zrtai',
     public: '28532'
 }
